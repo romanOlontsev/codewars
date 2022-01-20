985dcc77789d63cc6a8c5049a6e771fb5dfa63b1
romanOlontsev <roverviner@yandex.ru>
2022-01-20 15:04:02 +0300

    added  Create Phone Number skeleton, task and test

---
+++ src/com/codewars/tasks/createPhoneNumber/Kata.java
@@ -0,0 +1,8 @@
+package com.codewars.tasks.createPhoneNumber;
+
+public class Kata {
+    public static String createPhoneNumber(int[] numbers) {
+        // Your code here!
+        return null;
+    }
+}
