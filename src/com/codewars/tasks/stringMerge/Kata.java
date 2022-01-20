44ddfc2fbe02fd541937517742bb677d3acd3299
romanOlontsev <roverviner@yandex.ru>
2022-01-20 14:50:12 +0300

    add stingMerge task solution

--- src/com/codewars/tasks/stringMerge/Kata.java
+++ src/com/codewars/tasks/stringMerge/Kata.java
@@ -2,8 +2,8 @@ package com.codewars.tasks.stringMerge;
 
 public class Kata {
     public static String stringMerge(String s1, String s2, char letter) {
-        //write your adorable code here
-
-        return null;
+        String firstPath = s1.substring(0, s1.indexOf(letter));
+        String secondPath = s2.substring(s2.indexOf(letter));
+        return String.join("", firstPath, secondPath);
     }
 }
