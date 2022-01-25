c556b7ac65f333460e65957117c8be16184c408d
romanOlontsev <roverviner@yandex.ru>
2022-01-25 11:49:21 +0300

    add task solution

---
+++ src/com/codewars/tasks/jadenCasingStrings/JadenCase.java
@@ -0,0 +1,11 @@
+package com.codewars.tasks.jadenCasingStrings;
+
+public class JadenCase {
+
+    public String toJadenCase(String phrase) {
+        // TODO put your code below this comment
+
+        return null;
+    }
+
+}
