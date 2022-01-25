1581fe79ae8f514b74c13d921405077daad49ff3
romanOlontsev <roverviner@yandex.ru>
2022-01-25 12:11:46 +0300

    add task solution

--- src/com/codewars/tasks/jadenCasingStrings/JadenCase.java
+++ src/com/codewars/tasks/jadenCasingStrings/JadenCase.java
@@ -1,11 +1,15 @@
 package com.codewars.tasks.jadenCasingStrings;
 
 public class JadenCase {
-
     public String toJadenCase(String phrase) {
-        // TODO put your code below this comment
-
-        return null;
+        if (phrase == null || phrase.equals("")) {
+            return null;
+        }
+        String[] splitPhrase = phrase.split(" ");
+        for (int i = 0; i < splitPhrase.length; i++) {
+            char firstLetter =  splitPhrase[i].charAt(0);
+            splitPhrase[i] = splitPhrase[i].replace(firstLetter, Character.toUpperCase(firstLetter));
+        }
+        return String.join(" ", splitPhrase);
     }
-
 }
