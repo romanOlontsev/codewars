735bd4c814dcdc2c83009ab5df42f1052c4488c7
romanOlontsev <roverviner@yandex.ru>
2022-01-26 11:30:53 +0300

    add Take a Ten Minute Walk solution

--- src/com/codewars/tasks/takeATenMinuteWalk/TenMinWalk.java
+++ src/com/codewars/tasks/takeATenMinuteWalk/TenMinWalk.java
@@ -2,7 +2,24 @@ package com.codewars.tasks.takeATenMinuteWalk;
 
 public class TenMinWalk {
     public static boolean isValid(char[] walk) {
-        // Insert brilliant code here
-        return true;
+        int x = 0;
+        int y = 0;
+        int walkTime = 0;
+        for (char dir : walk) {
+            if (dir == 'n') {
+                y++;
+                walkTime++;
+            } else if (dir == 's') {
+                y--;
+                walkTime++;
+            } else if (dir == 'w') {
+                x--;
+                walkTime++;
+            } else if (dir == 'e') {
+                x++;
+                walkTime++;
+            }
+        }
+        return walkTime == 10 && x == 0 && y == 0;
     }
 }
