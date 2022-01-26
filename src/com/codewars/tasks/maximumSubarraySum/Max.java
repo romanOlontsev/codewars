3bc80cc80bc344b48a6be79ba1b51a7e55813d54
romanOlontsev <roverviner@yandex.ru>
2022-01-26 12:23:36 +0300

    add Maximum subarray sum solution

--- src/com/codewars/tasks/maximumSubarraySum/Max.java
+++ src/com/codewars/tasks/maximumSubarraySum/Max.java
@@ -2,6 +2,18 @@ package com.codewars.tasks.maximumSubarraySum;
 
 public class Max {
     public static int sequence(int[] arr) {
-        return 4;
+        if (arr == null || arr.length == 0) {
+            return 0;
+        }
+        int maxSumEver = 0;
+        int maxSumNow = 0;
+        for (int value : arr) {
+            maxSumNow = maxSumNow + value;
+            if (maxSumEver < maxSumNow)
+                maxSumEver = maxSumNow;
+            if (maxSumNow < 0)
+                maxSumNow = 0;
+        }
+        return maxSumEver;
     }
 }
