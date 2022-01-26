644a84c6dc3cba941e2e900e469ac89138862501
romanOlontsev <roverviner@yandex.ru>
2022-01-26 11:05:34 +0300

    add skeleton, task description and tests for the Take a Ten Minute Walk

---
+++ src/com/codewars/tasks/takeATenMinuteWalk/TenMinWalk.java
@@ -0,0 +1,8 @@
+package com.codewars.tasks.takeATenMinuteWalk;
+
+public class TenMinWalk {
+    public static boolean isValid(char[] walk) {
+        // Insert brilliant code here
+        return true;
+    }
+}
